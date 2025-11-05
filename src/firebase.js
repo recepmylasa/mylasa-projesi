e432cf917db783f97f78327a218eb260fcd45f1a
recepmylasa <karyahabermilas@gmail.com>
2025-11-05 22:59:21 +0300

    Günün çalışmaları tamamlandı

--- src/firebase.js
+++ src/firebase.js
@@ -9,12 +9,13 @@ const firebaseConfig = {
   apiKey: "AIzaSyAKNCsetEkPBBhBoeJTexyrYygL96vzMRo",
   authDomain: "mylasa-final.firebaseapp.com",
   projectId: "mylasa-final",
-  storageBucket: "mylasa-final.firebasestorage.app" // ← DOĞRU BUCKET
+  storageBucket: "mylasa-final.firebasestorage.app",   // ✅ DOĞRU
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app); // istersen: getStorage(app, "gs://mylasa-final.firebasestorage.app")
+export const storage = getStorage(app, "gs://mylasa-final.firebasestorage.app"); // ✅ DOĞRU
 export const functions = getFunctions(app, "europe-west3");
+export default app;
